b264d9579330fb1846facf44317a58d65e5491b8
Shitamori <shitamori1272@gmail.com>
2017-10-29 10:23:55 +0900

    add unity project

---
+++ Unity/MyoProject/Assets/LogMyoData.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LogMyoData : MonoBehaviour {
+
+    public GameObject targetCube;
+    StreamWriter writer;
+    float passed_time = 0f;
+    public string filename;
+
+	// Use this for initialization
+	void Start () {
+        FileInfo fi = new FileInfo(Application.dataPath + "/"+filename+".csv");
+        writer = fi.AppendText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        string log = "";
+        log += passed_time + ",";
+        log += targetCube.transform.position.x + ",";
+        log += targetCube.transform.position.y + ",";
+        log += targetCube.transform.position.z + ",";
+        log += targetCube.transform.rotation.x + ",";
+        log += targetCube.transform.rotation.y + ",";
+        log += targetCube.transform.rotation.z + ",";
+        log += targetCube.transform.rotation.w + ",";
+
+        writer.WriteLine(log);
+        writer.Flush();
+
+        passed_time += Time.deltaTime;
+
+
+    }
+}
---
+++ Unity/MyoProject/Assets/ShowTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ShowTime : MonoBehaviour {
+
+    public Slider timeSlider;
+    Text timetext;
+    DateTime time;
+    public int hour;
+    public int minute;
+    public int second;
+	// Use this for initialization
+	void Start () {
+
+        time = new DateTime(2017,10,29,hour,minute,second);
+        timetext = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timetext.text = time.AddSeconds((double)timeSlider.value).ToLongTimeString();
+	}
+}
