fbb8f40cc0f6a60cbf1d264270bea85411423543
Tomoki Oke <tomoki.oke@gmail.com>
2017-11-05 22:15:07 +0900

    a

--- server/main.py
+++ server/main.py
@@ -11,8 +11,23 @@ def index():
 @app.route('/user', methods=['POST'])
 def postUser():
     json = request.json
-    print(json['xxx'])
+    model.pushUser(
+            json['email'],
+            json['password']
+            )
     return jsonify(res = 'ok')
+
+@app.route('/log', methods=['POST'])
+def postLog():
+    json = request.json
+    model.pushLog(
+            json['user_id'],
+            json['lat'],
+            json['lng'],
+            json['created_at']
+            )
+    return jsonify(res = 'ok')
+
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True)
